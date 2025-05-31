6afaaebff02dcd8b4d64cc852f16fafc65bc6d53
Mauro Fernandez <maurofer87@gmail.com>
2025-05-31 18:35:13 -0300

    first commit

---
+++ scraper_mercadolibre.py
@@ -0,0 +1,11 @@
+# URL Base: https://listado.mercadolibre.com.ar/
+# Producto sugerido: Notebooks, smartphones, o electrodomésticos
+# Desafíos técnicos:
+# o Detección básica de bots
+# o Paginación con JavaScript
+# o Múltiples layouts de productos
+# o Rate limiting moderado
+
+
+# producto, precio, vendedor, ubicacion, reputacion_vendedor,
+# fecha_extraccion ,url _producto ,disponible ,envio_gratis, categoria
